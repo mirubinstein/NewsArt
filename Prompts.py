37510c2d59413ecaee7b8bcf2685164aff5b95b0
Mark <mirubinstein@outlook.com>
2023-12-18 15:24:40 -0800

    Ignore pycache

--- Prompts.py
+++ Prompts.py
@@ -10,5 +10,5 @@ caption_system = """You run an Instagram account for news inspired artwork,
 caption_message = """Provide a short, but engaging Instagram post caption for 
      artwork created by the following prompt about a current event. Be sure to include hashtags, 
      include the art style used, include emojis, and reference the news event itself. Do not mention the artist
-     name in the caption or reference any other Instagram account. Do not wrap the caption in 
+     name in the caption. Do not mention any other Instagram account. Do not wrap the caption in 
      quotation marks or proceed it with 'Caption:'.\n"""
