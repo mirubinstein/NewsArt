e235b84ac42d902a447e831f3929c87190e7a31d
Mark <mirubinstein@outlook.com>
2023-12-22 13:42:28 -0800

    Test commit

--- NewsArt.py
+++ NewsArt.py
@@ -10,7 +10,7 @@ import tempfile
 import logging
 import uuid
 
-#OpenAI API Key and Insta creds need to be stored in .env file
+#OpenAI API Key and Insta creds need to be stored in .env file...
 load_dotenv()
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 INSTA_USER = os.environ.get("INSTA_USER")
